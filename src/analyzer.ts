9b5535eb8c01bd089978ff50eab6d22110077722
aarish mansur <aarishmansur@gmail.com>
2026-07-09 19:16:48 +0530

    added ts-morph

--- src/analyzer.ts
+++ src/analyzer.ts
@@ -1,20 +1,30 @@
-import fs from "fs"
+import * as fs from "fs"
+import * as path from "path"
+import { Project } from "ts-morph"
 
-export const getAllDependencies  =  async() =>{
-    try {
-         const data = await fs.readFileSync("./package.json",'utf-8')
-         const  packageObj = JSON.parse(data)
-         const prodDeps = packageObj.dependencies ? Object.keys(packageObj.dependencies) : []
-         const devDeps = packageObj.dependencies ? Object.keys(packageObj.devDependencies) : []
-          
-        const all_dependencies  = [...prodDeps,...devDeps];
-        console.log("All dependencies",all_dependencies);
-        
-
-    } catch (error) {
-        console.error("Couldnt get the depenendeis")
-    }
+interface DeclaredDeps{
+    proudDeps : string[],
+    devDeps:string[],
+    allDeclared:Set<string[]>,
+    duplicatedDeps:string[]
 }
 
+interface AnalysisReport{
+    used:string[],
+    unusued : string[],
+    duplicatd:string[],
+}
 
-getAllDependencies()
+export const getAllDependencies  = async(packageJsonPath:string)=>{
+    const resolvePath =  path.resolve(packageJsonPath);
+    if(!resolvePath) {
+        throw new  Error("Could not find package.json  in directory")
+    }
+    const packageData = JSON.parse(fs.readFileSync(resolvePath,'utf-8'))
+    const prodDeps = packageData.dependencies ? Object.keys(packageData.dependencies) : []
+    const devDeps = packageData.devdependencies ? Object.keys(packageData.devdependencies):[]
+    const allDeclared  = new  Set([...prodDeps,...devDeps])
+     const duplicates = prodDeps.filter(dep=>devDeps.includes(dep))
+     return { prodDeps, devDeps, allDeclared, duplicates };
+}
+
