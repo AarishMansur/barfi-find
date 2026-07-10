d32f88cfd5be7e5b7b86b5e6f5c3ab85230d5917
aarish mansur <aarishmansur@gmail.com>
2026-07-10 19:45:05 +0530

    wrote the getImpotedpackage logic function

--- src/analyzer.ts
+++ src/analyzer.ts
@@ -1,11 +1,11 @@
 import * as fs from "fs"
 import * as path from "path"
-import { Project } from "ts-morph"
+import { Project,SyntaxKind } from "ts-morph"
 
 interface DeclaredDeps{
-    proudDeps : string[],
+    prodDeps : string[],
     devDeps:string[],
-    allDeclared:Set<string[]>,
+    allDeclared:Set<string>,
     duplicatedDeps:string[]
 }
 
@@ -15,7 +15,7 @@ interface AnalysisReport{
     duplicatd:string[],
 }
 
-export const getAllDependencies  = async(packageJsonPath:string)=>{
+export const getAllDependencies  = (packageJsonPath:string = "./package.json"): DeclaredDeps =>{
     const resolvePath =  path.resolve(packageJsonPath);
     if(!resolvePath) {
         throw new  Error("Could not find package.json  in directory")
@@ -24,8 +24,9 @@ export const getAllDependencies  = async(packageJsonPath:string)=>{
     const prodDeps = packageData.dependencies ? Object.keys(packageData.dependencies) : []
     const devDeps = packageData.devdependencies ? Object.keys(packageData.devdependencies):[]
     const allDeclared  = new  Set([...prodDeps,...devDeps])
-     const duplicates = prodDeps.filter(dep=>devDeps.includes(dep))
-     return { prodDeps, devDeps, allDeclared, duplicates };
+     const duplicatedDeps = prodDeps.filter(dep=>devDeps.includes(dep)) 
+
+     return { prodDeps, devDeps, allDeclared, duplicatedDeps };
 }
 
 
@@ -34,9 +35,47 @@ export  const getImportedPackages  = (fileGlobPattern:string = "src/**/*{.ts,.ts
     project.addSourceFileAtPath(fileGlobPattern)
     
     const importedPackages = new Set<string>()
-    
-    // from here write  Helper to extract base name
 
+    // Helper to extract base names (e.g., "lodash/fp" -> "lodash", "@types/node" -> "@types/node")
+    const getBasePackege = (importstring:string):string=>{
+     if(importstring.startsWith(".") || importstring.startsWith("/")) return "";
+     const parts = importstring.split("/")
+     return importstring.startsWith("@") ? `${parts[0]}/${parts[1]}` : `${parts[0]}`
+    }
+
+    // Standard ES Imports (import x from 'y')
+    const sourceFiles = project.getSourceFiles();
+
+     for(const sourceFile of sourceFiles){
+       sourceFile.getImportDeclarations().forEach(imp => {
+        const pkg = getBasePackege(imp.getModuleSpecifierValue())
+        if(pkg) importedPackages.add(pkg)
+       });
+
+       // ES Export From statements (export * from 'y')
+      sourceFile.getExportDeclarations().forEach(exp=>{
+          const specifer  = exp.getModuleSpecifierValue()
+          if(specifer){
+            const pkg = getBasePackege(specifer)
+            if(pkg) importedPackages.add(pkg)
+          }
+      })
+
+       // CommonJS require() and Dynamic import()
+       sourceFile.getDescendantsOfKind(SyntaxKind.CallExpression).forEach(call=>{
+           const called = call.getExpression().getText()
+           if(called ==="require" || called ==="import"){
+            const args  = call.getArguments()
+            if(args.length >0 && args[0]?.getKindName() ==="StringLiteral"){
+                const pkg = getBasePackege(args[0].getText().replace(/['"]/g, ""))
+                if(pkg) importedPackages.add(pkg)
+            }
+           }
+       })
+     }
+
+     
+      
 
     return importedPackages;
 }
